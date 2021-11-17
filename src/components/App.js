2344a8b7f03233aedce351d85338c66398b46109
+ <brandi.ude@gmail.com>
2021-11-17 12:34:22 -0500

    commit tree 9da84594fa5d36d5333296673b01e8e5a557d094

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
+// remove a listing from page by clicking the trash can icon. This change should be persisted in the backend.
+
 function App() {
+  const [listings, setListings]=useState([])
+  const [search, setSearch]=useState("")
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then(r=> r.json())
+    .then(setListings)
+  }, [])
+
+  function removeListing(id){
+    const updatedListings = listings.filter((listing) => listing.id !== id);
+    setListings(updatedListings);
+  }
+
+  const displayedListings = listings.filter((listing) =>
+  listing.description.toLowerCase().includes(search.toLowerCase())
+);
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={setSearch}/>
+      <ListingsContainer listings={displayedListings} removeListing={removeListing}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({listings, removeListing}) {
+
   return (
     <main>
       <ul className="cards">
+        {listings.map((listing) => (
+        <ListingCard key={listing.id} listing={listing} removeListing={removeListing}/>
+        ))}
         {/* use the ListingCard component to display listings */}
       </ul>
     </main>
